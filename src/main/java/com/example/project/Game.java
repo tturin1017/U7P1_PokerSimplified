43aeeb16c3834e47227867848df5667640156857
Tatiana Turin <146445767+tturin1017@users.noreply.github.com>
2025-02-26 15:01:35 +0000

    fnal

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -6,11 +6,14 @@ public class Game{
     public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
         int p1Result = Utility.getHandRanking(p1Hand);
         int p2Result = Utility.getHandRanking(p2Hand);
+        String p1wins = "Player 1 wins!";
+        String p2wins = "Player 2 wins!";
+        String tie = "Tie!";
 
         if(p1Result>p2Result){
-            return "Player 1 wins!"; //player two wins!
+            return p1wins; //player two wins!
         }else if(p1Result<p2Result){
-            return "Player 2 wins!";
+            return p2wins;
         }else{//we have a draw, we must determine who wins 
             //impossible for two players to both get royal flush 
             //impossible for two players to both get a straight flush 
@@ -25,18 +28,10 @@ public class Game{
             ArrayList<Integer> p1_rankFreqList = p1.findRankingFrequency();
             ArrayList<Integer> p2_rankFreqList = p2.findRankingFrequency();
             String p1_rank=""; String p2_rank="";
-            String message;
-            if(p1_max>p2_max){
-                message = "Player 1 wins!";
-            }else if(p2_max>p1_max){
-                message = "Player 2 wins!";
-            }else{
-                message="Tie!";
-            }
-
-            //possible for two players to get full house 3p and 2p
-            //community cards must be 3p so deteremine who has the higher pair
-            if(p1Result== 8 && p2Result == 8){ //FULL HOUSE DRAW
+            String message = determineMaxWinner(p1_max, p2_max);
+     
+            
+            if(p1Result== 8 && p2Result == 8){ //FULL HOUSE DRAW only possible is community cards is 3 of a kind, so find the max of the hands
                 return message;
             }else if(p1Result == 7 && p2Result == 7){ //FLUSH DRAW. Determine who has the higher card in hand
                 return message;
@@ -52,14 +47,13 @@ public class Game{
                     }
                 }
                 if(Utility.getRankValue(p1_rank)>Utility.getRankValue(p2_rank)){
-                    return "Player 1 wins!";
+                    return p1wins;
                 }else if(Utility.getRankValue(p1_rank)<Utility.getRankValue(p2_rank)){
-                    return "Player 2 wins!";
+                    return p2wins;
                 }else{ //3 pair is the community cards so find the highest card 
                     return message; //max  between cards is calculated
                 }
             }else if(p1Result == 4 && p2Result == 4){ //TWO PAIR DRAW
-                //scenario 1-> 1 pair is the in the community deck. Check for this
                 int p1_i_firstPair  =-1; int p1_i_secondPair = -1; int p2_i_firstPair = -1; int p2_i_secondPair = -1;
                 for(int i=0;i<p1_rankFreqList.size();i++){
                     if(p1_rankFreqList.get(i)==2){
@@ -79,28 +73,19 @@ public class Game{
                 }
 
                 int p1_firstPair_val = Utility.getRankValue(Utility.getRanks()[p1_i_firstPair]);
-                int p1_secondPair_val = Utility.getRankValue(Utility.getRanks()[p1_i_firstPair]);
-                int p1_firstPair_val = Utility.getRankValue(Utility.getRanks()[p1_i_firstPair]);
-                int p1_firstPair_val = Utility.getRankValue(Utility.getRanks()[p1_i_firstPair]);
+                int p1_secondPair_val = Utility.getRankValue(Utility.getRanks()[p1_i_secondPair]);
+                int p2_firstPair_val = Utility.getRankValue(Utility.getRanks()[p2_i_firstPair]);
+                int p2_secondPair_val = Utility.getRankValue(Utility.getRanks()[p2_i_secondPair]);
 
-                if (p1_i_firstPair == p2_i_firstPair){
+                if (p1_firstPair_val == p2_firstPair_val){
                     //check other two pairs to determine who is higher 
-                    
-                }
-                
-                //|| p1_i_firstPair == p2_i_secondPair || p1_i_secondPair == p2_i_firstPair || p1_i_secondPair == p2_i_secondPair){
-                    //community card is a pair
-                    //check 
-                }
-                //now compare rank
-                int x = Utility.getRankValue(Utility.getRanks()[i1]); 
-                int y  = Utility.getRankValue(Utility.getRanks()[i2]); 
-                if(x>y){
-                    return "Player 1 wins!"; //two pair draw
-                }else if(x>y){
-                    return "Player 2 wins!";
-                }else{
-                    return "Tie!";
+                    return determineMaxWinner(p1_secondPair_val, p2_secondPair_val);
+                }else if(p1_firstPair_val == p2_secondPair_val){
+                    return determineMaxWinner(p1_secondPair_val,p2_firstPair_val);
+                }else if (p1_secondPair_val == p2_firstPair_val){
+                    return determineMaxWinner(p1_firstPair_val,p2_secondPair_val);
+                }else if (p1_secondPair_val == p2_secondPair_val){
+                    return determineMaxWinner(p1_firstPair_val, p2_firstPair_val);
                 }
             }else if(p1Result == 3 && p2Result == 3){ //PAIR DRAW. Find out which pair is the highest
                 for(int i =0 ; i<p1_rankFreqList.size();i++){
@@ -129,8 +114,21 @@ public class Game{
             return c1;
         }else if (c2>c1){
             return c2;
+        }else{ //its a tie 
+            return c1;
         }
-        return -1;
+    }
+
+    public static String determineMaxWinner(int p1, int p2){
+        System.out.println(p1);
+        System.out.println(p2);
+        if(p1>p2){
+            return "Player 1 wins!";
+        }else if(p1<p2){
+            return "Player 2 wins!";
+        }else{
+            return "Tie!";
+        }
     }
 
 
@@ -174,20 +172,35 @@ public class Game{
         }
         
         public static void main(String[] args) {
-            //play();
-            Player player = new Player();
-            player.addCard(new Card("7", "♠"));
-            player.addCard(new Card("8", "♣"));
+          
+            Player player1 = new Player();
+            Player player2 = new Player();
             
-            // Community Cards
+            // Player 1 has a Full House (7s full of 8s)
+            player1.addCard(new Card("7", "♠"));
+            player1.addCard(new Card("7", "♣"));
+            
+            // Player 2 has Three of a Kind (8s)
+            player2.addCard(new Card("8", "♠"));
+            player2.addCard(new Card("8", "♣"));
+            
             ArrayList<Card> communityCards = new ArrayList<>();
             communityCards.add(new Card("7", "♦"));
             communityCards.add(new Card("8", "♥"));
             communityCards.add(new Card("A", "♠"));
             
-            player.playHand(communityCards);
-            String handResult = player.playHand(communityCards);
-            System.out.println(handResult);
+            String p1Result = player1.playHand(communityCards);
+            String p2Result = player2.playHand(communityCards);
+
+            System.out.println(player1.findRankingFrequency());
+            System.out.println(player2.findRankingFrequency());
+            System.out.println(player1.findSuitFrequency());
+            System.out.println(player2.findSuitFrequency());
+            
+            
+            String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
+            System.out.println(winner);
+            
     }
 
 }
--- src/test/java/com/example/project/TestGame.java
+++ src/test/java/com/example/project/TestGame.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 public class TestGame {
-    @Test
-    public void testDetermineWinnerPlayer1Wins() {
+
+    @Test // Test case where Player 1 wins with a pair
+    public void testAPair_Player1Wins() {
         Player player1 = new Player();
         Player player2 = new Player();
         
@@ -25,14 +26,13 @@ public class TestGame {
         
         String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
         
-        assertEquals("Player 1 wins!", winner); // Player 1 should win (high card Ace)
+        assertEquals("Player 1 wins!", winner); // Player 1 should win with a pair of Aces
     }
 
-    @Test
-    public void testDetermineWinnerPlayer2Wins() {
+    @Test // Test case where Player 2 wins with higher high card
+    public void testHighCard_Player2Wins() {
         Player player1 = new Player();
         Player player2 = new Player();
-        //two pair determine high card winner
         
         player1.addCard(new Card("7", "♠"));
         player1.addCard(new Card("8", "♠"));
@@ -41,8 +41,8 @@ public class TestGame {
         player2.addCard(new Card("10", "♠"));
         
         ArrayList<Card> communityCards = new ArrayList<>();
-        communityCards.add(new Card("7", "♦"));
-        communityCards.add(new Card("8", "♣"));
+        communityCards.add(new Card("Q", "♦"));
+        communityCards.add(new Card("J", "♣"));
         communityCards.add(new Card("A", "♠"));
         
         String p1Result = player1.playHand(communityCards);
@@ -53,12 +53,11 @@ public class TestGame {
         assertEquals("Player 2 wins!", winner); // Player 2 should win (high card 10)
     }
 
-    @Test
-    public void testTieGame() {
+    @Test // Test case where it's a tie with equal high cards
+    public void testHighCard_TieGame() {
         Player player1 = new Player();
         Player player2 = new Player();
 
-        //high card draw
         player1.addCard(new Card("K", "♠"));
         player1.addCard(new Card("Q", "♠"));
         
@@ -78,53 +77,72 @@ public class TestGame {
         assertEquals("Tie!", winner); // It's a tie between both players
     }
 
-    @Test
-    public void testDetermineWinnerWithFullHouse() {
+    @Test // Test case where Player 1 wins with higher straight
+    public void testStraight_Player1Wins() {
         Player player1 = new Player();
         Player player2 = new Player();
         
-        // Player 1 has a Full House
-        player1.addCard(new Card("7", "♠"));
-        player1.addCard(new Card("7", "♣"));
+        // Player 1 has a straight: 7,8,9,10,J
+        player1.addCard(new Card("10", "♠"));
+        player1.addCard(new Card("J", "♠"));
         
-        // Player 2 has a Full House
-        player2.addCard(new Card("8", "♠"));
-        player2.addCard(new Card("8", "♣"));
+        // Player 2 has a straight: 5, 6, 7, 8, 9
+        player2.addCard(new Card("5", "♠"));
+        player2.addCard(new Card("6", "♠"));
         
         ArrayList<Card> communityCards = new ArrayList<>();
         communityCards.add(new Card("7", "♦"));
-        communityCards.add(new Card("8", "♥"));
-        communityCards.add(new Card("A", "♠"));
+        communityCards.add(new Card("8", "♠"));
+        communityCards.add(new Card("9", "♥"));
         
         String p1Result = player1.playHand(communityCards);
         String p2Result = player2.playHand(communityCards);
         
         String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
         
-        assertEquals("Player 2 wins! Full House Draw", winner); // Player 2 wins with a higher Full House
+        assertEquals("Player 1 wins!", winner); // Player 1 should win with the higher straight (7,8,9,10,J)
     }
 
-
-
-    // 1. Test for "Player 1 wins!" due to higher hand ranking. full house vs 3 of a kind
-    @Test
-    public void testDetermineWinner_Player1WinsByHigherHand() {
+    @Test // Test case where Player 1 wins with higher Full House
+    public void testFullHouseDraw_Player1Wins() {
         Player player1 = new Player();
         Player player2 = new Player();
         
-        // Player 1 has a Full House (7s full of 8s)
-        player1.addCard(new Card("7", "♠"));
-        player1.addCard(new Card("7", "♣"));
+        player1.addCard(new Card("A", "♠"));
+        player1.addCard(new Card("A", "♣"));
         
-        // Player 2 has Three of a Kind (8s)
         player2.addCard(new Card("8", "♠"));
         player2.addCard(new Card("8", "♣"));
         
         ArrayList<Card> communityCards = new ArrayList<>();
         communityCards.add(new Card("7", "♦"));
-        communityCards.add(new Card("8", "♥"));
-        communityCards.add(new Card("A", "♠"));
+        communityCards.add(new Card("7", "♥"));
+        communityCards.add(new Card("7", "♠"));
         
+        String p1Result = player1.playHand(communityCards);
+        String p2Result = player2.playHand(communityCards);
+        
+        String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
+        
+        assertEquals("Player 1 wins!", winner); // Player 1 wins with a higher Full House
+    }
+
+    @Test // Test case where Player 1 wins with Full House vs Three of a Kind
+    public void testFullHouseVsThreeOfAKind_Player1Wins() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        
+        player1.addCard(new Card("7", "♠"));
+        player1.addCard(new Card("7", "♣"));
+        
+        player2.addCard(new Card("8", "♠"));
+        player2.addCard(new Card("3", "♣"));
+        
+        ArrayList<Card> communityCards = new ArrayList<>();
+        communityCards.add(new Card("A", "♦"));
+        communityCards.add(new Card("A", "♥"));
+        communityCards.add(new Card("A", "♠"));
+    
         String p1Result = player1.playHand(communityCards);
         String p2Result = player2.playHand(communityCards);
         
@@ -133,44 +151,16 @@ public class TestGame {
         assertEquals("Player 1 wins!", winner); // Player 1 wins with Full House
     }
 
-    // 2. Test for "Player 2 wins!" due to higher hand ranking. Both players have a full house but player 2 has a higher pair
-    @Test
-    public void testDetermineWinner_Player2WinsByHigherHand() {
-        Player player1 = new Player();
-        Player player2 = new Player();
-        
-        // Player 1 has a Full House (7s full of 8s)
-        player1.addCard(new Card("7", "♠"));
-        player1.addCard(new Card("7", "♣"));
-        
-        // Player 2 has a Full House (8s full of 7s)
-        player2.addCard(new Card("8", "♠"));
-        player2.addCard(new Card("8", "♣"));
-        
-        ArrayList<Card> communityCards = new ArrayList<>();
-        communityCards.add(new Card("7", "♦"));
-        communityCards.add(new Card("8", "♥"));
-        communityCards.add(new Card("A", "♠"));
-        
-        String p1Result = player1.playHand(communityCards);
-        String p2Result = player2.playHand(communityCards);
-        
-        String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
-        
-        assertEquals("Player 2 wins!", winner); 
-    }
 
-    // 3. Test for "Tie!" message when both players have identical rankings and highest cards.
-    @Test
-    public void testDetermineWinner_TieDueToEqualHands() {
+
+    @Test // Test case for Tie in Two Pair scenario. Both have a full house
+    public void testTwoPair_TieGame() {
         Player player1 = new Player();
         Player player2 = new Player();
         
-        // Player 1 has Two Pair (8s and 7s)
         player1.addCard(new Card("8", "♠"));
         player1.addCard(new Card("7", "♠"));
         
-        // Player 2 has Two Pair (8s and 7s)
         player2.addCard(new Card("8", "♠"));
         player2.addCard(new Card("7", "♣"));
         
@@ -187,17 +177,14 @@ public class TestGame {
         assertEquals("Tie!", winner); 
     }
 
-    // 4. Test for "Player 1 wins!" due to higher rank in Three of a Kind draw.
-    @Test
-    public void testDetermineWinner_Player1WinsInThreeOfAKindDraw() {
+    @Test // Test case where Player 1 wins with higher Three of a Kind
+    public void testThreeOfAKind_Player1Wins() {
         Player player1 = new Player();
         Player player2 = new Player();
         
-        // Player 1 has Three of a Kind (Aces)
         player1.addCard(new Card("A", "♠"));
         player1.addCard(new Card("A", "♣"));
         
-        // Player 2 has Three of a Kind (Kings)
         player2.addCard(new Card("K", "♠"));
         player2.addCard(new Card("K", "♣"));
         
@@ -214,86 +201,85 @@ public class TestGame {
         assertEquals("Player 1 wins!", winner); 
     }
 
-    // 5. Test for "Player 2 wins!" due to higher rank in Two Pair draw.
-    @Test
-    public void testDetermineWinner_Player2WinsInTwoPairDraw() {
+    @Test //Player 2 wins with Four of a Kind vs Three of a Kind
+    public void testFourOfAKindVsThreeOfAKind_Player2Wins() {
         Player player1 = new Player();
         Player player2 = new Player();
         
-        // Player 1 has Two Pair (9s and 7s)
-        player1.addCard(new Card("9", "♠"));
-        player1.addCard(new Card("7", "♠"));
-        
-        // Player 2 has Two Pair (10s and 7s)
-        player2.addCard(new Card("10", "♠"));
-        player2.addCard(new Card("7", "♠"));
-        
-        ArrayList<Card> communityCards = new ArrayList<>();
-        communityCards.add(new Card("9", "♦"));
-        communityCards.add(new Card("10", "♠"));
-        communityCards.add(new Card("7", "♣"));
-        
-        String p1Result = player1.playHand(communityCards);
-        String p2Result = player2.playHand(communityCards);
-        
-        String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
-        
-        assertEquals("Player 2 wins!", winner); 
-    }
-
-    // 6. Test for "Player 1 wins!" due to higher card in High Card draw.
-    @Test
-    public void testDetermineWinner_Player1WinsInHighCardDraw() {
-        Player player1 = new Player();
-        Player player2 = new Player();
-        
-        // Player 1 has a High Card (Ace)
-        player1.addCard(new Card("A", "♠"));
-        player1.addCard(new Card("2", "♠"));
-        
-        // Player 2 has a High Card (King)
-        player2.addCard(new Card("K", "♠"));
-        player2.addCard(new Card("3", "♠"));
-        
-        ArrayList<Card> communityCards = new ArrayList<>();
-        communityCards.add(new Card("7", "♦"));
-        communityCards.add(new Card("8", "♠"));
-        communityCards.add(new Card("9", "♣"));
-        
-        String p1Result = player1.playHand(communityCards);
-        String p2Result = player2.playHand(communityCards);
-        
-        String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
-        
-        assertEquals("Player 1 wins!", winner); 
-    }
-
-    // 7. Test for "Player 2 wins!" when they have the higher high card in a High Card draw.
-    @Test
-    public void testDetermineWinner_Player2WinsInHighCardDraw() {
-        Player player1 = new Player();
-        Player player2 = new Player();
-        
-        // Player 1 has a High Card (Jack)
+        // Player 1 has Three of a Kind: 3 Jacks
         player1.addCard(new Card("J", "♠"));
-        player1.addCard(new Card("4", "♠"));
+        player1.addCard(new Card("J", "♣"));
         
-        // Player 2 has a High Card (Queen)
-        player2.addCard(new Card("Q", "♠"));
-        player2.addCard(new Card("2", "♠"));
+        // Player 2 has Four of a Kind: 4 Tens
+        player2.addCard(new Card("10", "♠"));
+        player2.addCard(new Card("10", "♣"));
         
         ArrayList<Card> communityCards = new ArrayList<>();
-        communityCards.add(new Card("7", "♦"));
-        communityCards.add(new Card("8", "♠"));
-        communityCards.add(new Card("9", "♣"));
+        communityCards.add(new Card("10", "♦"));
+        communityCards.add(new Card("10", "♥"));
+        communityCards.add(new Card("J", "♠"));
         
         String p1Result = player1.playHand(communityCards);
         String p2Result = player2.playHand(communityCards);
         
         String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
         
-        assertEquals("Player 2 wins!", winner); // Player 2 wins with Queen high
+        assertEquals("Player 2 wins!", winner); // Player 2 wins with Four of a Kind (Tens)
     }
-    
-   
+
+    @Test //Player 1 wins with Straight Flush
+    public void testStraightFlush_Player1Wins() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        
+        // Player 1 has a Straight Flush: 5, 6, 7, 8, 9 of Spades
+        player1.addCard(new Card("5", "♠"));
+        player1.addCard(new Card("6", "♠"));
+        
+        // Player 2 has a Flush: 2, 3, 4, 5, 6 of Hearts (but no Straight Flush)
+        player2.addCard(new Card("2", "♥"));
+        player2.addCard(new Card("3", "♥"));
+        
+        ArrayList<Card> communityCards = new ArrayList<>();
+        communityCards.add(new Card("7", "♠"));
+        communityCards.add(new Card("8", "♠"));
+        communityCards.add(new Card("9", "♠"));
+        
+        String p1Result = player1.playHand(communityCards);
+        String p2Result = player2.playHand(communityCards);
+        
+        String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
+        
+        assertEquals("Player 1 wins!", winner); // Player 1 wins with a Straight Flush (5♠, 6♠, 7♠, 8♠, 9♠)
+    }
+
+    @Test // Flush Draw test case
+    public void testFlushDraw_Player2Wins() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        
+        player1.addCard(new Card("7", "♠"));
+        player1.addCard(new Card("10", "♠"));
+  
+        player2.addCard(new Card("A", "♠"));
+        player2.addCard(new Card("3", "♠"));
+
+        
+        // Community cards that could help form the flush
+        ArrayList<Card> communityCards = new ArrayList<>();
+        communityCards.add(new Card("J", "♠")); // Player 1 completes the flush with this card
+        communityCards.add(new Card("4", "♠"));
+        communityCards.add(new Card("Q", "♠"));
+        
+        // Player results after playing the hand
+        String p1Result = player1.playHand(communityCards);
+        String p2Result = player2.playHand(communityCards);
+        
+        // Determine the winner
+        String winner = Game.determineWinner(player1, player2, p1Result, p2Result, communityCards);
+        
+        // Player 1 should win 
+        assertEquals("Player 2 wins!", winner);
+    }   
+
 }
